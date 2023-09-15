6494437468b2101ae70a73b39d7fce7a14bd4154
Tianyi Pan <tianyi.pan@clobotics.com>
2023-09-15 17:02:06 +0800

    First working demo

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 import asyncio
 import streamlit as st
+from inference import *
 from pathlib import Path
 
 FILE_ROOT = Path(__file__).parent
@@ -9,15 +11,32 @@ SUPPORTED_MODELS = {
     "gpt-3.5-turbo": {  # If you deployed on Azure, set the environment variables as below, and just ignore it if you deployed on OpenAI
         "model_name": os.getenv("AZURE_CHATGPT_MODEL_NAME", "gpt-3.5-turbo"),
         "model_engine": os.getenv("AZURE_CHATGPT_DEPLOYMENT_NAME", None),
+        "model_func": call_openai,
+        "avatar": "https://openai.com/favicon.ico",
         "stream": True
     },
-    "llama-2-7b": {"stream": True},
-    "llama-2-13b": {"stream": True},
-    "llama-2-70b": {"stream": True},
-    "dolly-v2-12b": {"stream": False},
-    "falcon-7b": {"stream": False},
-    "pythia-12b": {"stream": False},
-    "pythia-2.8b": {"stream": False},
+    "llama-2-7b": {
+        "model_name": "meta-llama/Llama-2-7b-chat-hf",
+        "model_func": call_deepinfra,
+        "avatar": "https://deepinfra.com/deepinfra-logo-64.webp",
+        "stream": True
+    },
+    "llama-2-13b": {
+        "model_name": "meta-llama/Llama-2-13b-chat-hf",
+        "model_func": call_deepinfra,
+        "avatar": "https://deepinfra.com/deepinfra-logo-64.webp",
+        "stream": True
+    },
+    "llama-2-70b": {
+        "model_name": "meta-llama/Llama-2-70b-chat-hf",
+        "model_func": call_deepinfra,
+        "avatar": "https://deepinfra.com/deepinfra-logo-64.webp",
+        "stream": True
+    },
+    # "dolly-v2-12b": {"stream": False},
+    # "falcon-7b": {"stream": False},
+    # "pythia-12b": {"stream": False},
+    # "pythia-2.8b": {"stream": False},
 }
 
 @st.cache_data(show_spinner=False)
@@ -25,16 +44,64 @@ def get_css() -> str:
     # Read CSS code from style.css file
     with open(FILE_ROOT / "style.css", "r") as f:
         return f"<style>{f.read()}</style>"
+    
+def render_messages(messages: list, assistant_avatar: str | None = None):
+    for message in messages:
+        match message["role"]:
+            case "user":
+                with st.chat_message("user"):
+                    st.markdown(message["content"])
+            case "assistant":
+                if assistant_avatar is None:
+                    with st.chat_message("assistant"):
+                        st.markdown(message["content"])
+                else:
+                    with st.chat_message("assistant", avatar=assistant_avatar):
+                        st.markdown(message["content"])
 
-async def _get_reply(container: st.container, messages: list, model_func: callable, model_func_kwargs: dict):
+async def get_reply(
+    container: st.container,
+    messages_key: str,
+    prompt: str,
+    model_settings: dict,
+):
     with container:
-        st.write(f"You wrote:")
-        reply_box = st.empty()
-        reply_text = ""
-        for char in prompt:
-            reply_text += char
-            reply_box.write(reply_text)
-            await asyncio.sleep(0.1)
+        stream = model_settings.get("stream", False)
+        
+        # Render new human prompt
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Add prompt to chat history
+        st.session_state.messages[messages_key].append({"role": "user", "content": prompt})
+
+        # Render new response
+        with st.chat_message("assistant", avatar=model_settings.get("avatar", None)):
+            message_placeholder = st.empty()
+            full_response = ""
+            message_placeholder.markdown(full_response + "▌")
+            if stream:
+                async for chunk in model_settings["model_func"](
+                    messages=st.session_state.messages[messages_key],
+                    model_settings=model_settings,
+                ):
+                    if chunk is not None:
+                        full_response += chunk
+                        message_placeholder.markdown(full_response + "▌")
+                message_placeholder.markdown(full_response)
+            else:
+                response = model_settings["model_func"](
+                    messages=st.session_state.messages[messages_key],
+                    model_settings=model_settings,
+                )
+                for chunk in response.split():
+                    full_response += chunk + " "
+                    message_placeholder.markdown(full_response + "▌")
+                    await asyncio.sleep(0.05)
+                message_placeholder.markdown(full_response)
+
+        # Add response to chat history
+        st.session_state.messages[messages_key].append({"role": "assistant", "content": full_response})
 
 async def main():
     st.set_page_config(
@@ -53,29 +120,47 @@ async def main():
 
     st.write("Select up to four different models to compare side-by-side:")
 
+    # Number of models to compare
     n_models = st.number_input("Number of models", 1, 4, 2, 1)
 
+    # User-entered prompt for the models
+    if n_models > 1:
+        placeholder_text = f"Send a message to all {n_models} models"
+    else:
+        placeholder_text = "Send a message to the model"
+
+    with st.form("prompt_form", clear_on_submit=True):
+        prompt = st.text_area(
+            "Prompt",
+            placeholder=placeholder_text, 
+            label_visibility="collapsed",
+            height=50,
+        )
+        prompt_submitted = st.form_submit_button("Send")
+    if st.button("Clear all message histories"):
+        st.session_state.messages = {}
+
+    # Render model columns
     columns = st.columns(n_models)
 
     for i, column in enumerate(columns):
         model = column.selectbox(f"Model {i+1}", sorted(SUPPORTED_MODELS.keys()), key=f"model_{i}")
         if f"{model}_{i}" not in st.session_state.messages:
             st.session_state.messages[f"{model}_{i}"] = []
+        else:
+            with column:
+                render_messages(
+                    st.session_state.messages[f"{model}_{i}"], 
+                    SUPPORTED_MODELS[model].get("avatar", None)
+                )
 
-    if n_models > 1:
-        placeholder_text = f"Send a message to all {n_models} models"
-    else:
-        placeholder_text = "Send a message to the model"
-    prompt = st.chat_input(placeholder_text)
-
-    if prompt:
+    if prompt_submitted:
         # Collect async tasks for all columns and run them simultaneously using gather
         coroutines = []
         for i in range(n_models):
             model = st.session_state[f"model_{i}"]
-            messages = st.session_state.messages[f"{model}_{i}"]
-            messages.append({"role": "user", "content": prompt})
-            coroutines.append(_get_reply(columns[i], messages, None))
+            messages_key = f"{model}_{i}"
+            coroutines.append(get_reply(columns[i], messages_key, prompt, SUPPORTED_MODELS[model]))
         await asyncio.gather(*coroutines)
 
 if __name__ == "__main__":
--- src/inference.py
+++ src/inference.py
@@ -1,27 +1,42 @@
 # A collection of inference functions to be called from the main Streamlit app
 
 import os
+import json
 import openai
 import aiohttp
 from settings import *
 
+def convert_openai_messages_to_prompt(messages: list) -> str:
+    # Convert the OpenAI standard messages format into a text prompt for models which don't support it
+    prompt = "Below is a conversation between a human (user) and an AI (assistant). Please continue it by one reply.\n"
+    for message in messages:
+        match message["role"]:
+            case "user":
+                prompt += f"User: {message['content']}\n"
+            case "assistant":
+                prompt += f"Assistant: {message['content']}\n"
+            # For now, we ignore other possible roles, but may revisit the logic later
+    prompt += "Assistant: "
+    return prompt
+
+
 async def call_openai(
-    model: dict,
     messages: list,
-    stream: bool = False,
+    model_settings: dict,
 ) -> str | None:
     try:
         # Just make sure the environment variables are set, OpenAI API can get them automatically
         os.getenv("OPENAI_API_KEY")
     except KeyError as e:
         raise RuntimeError(f"Missing environment variable: {e}")
-    
+
+    stream = model_settings.get("stream", False)
     if stream:
         async for chunk in await openai.ChatCompletion.acreate(
-            model=model["model_name"],
-            engine=model["model_engine"],
+            model=model_settings["model_name"],
+            engine=model_settings["model_engine"],
             messages=messages,
-            max_tokens=model.get("max_reply_tokens", LLM_MAX_REPLY_TOKENS),
+            max_tokens=model_settings.get("max_reply_tokens", LLM_MAX_REPLY_TOKENS),
             stream=stream,
         ):
             content = chunk["choices"][0].get("delta", {}).get("content", None)
@@ -29,27 +44,63 @@ async def call_openai(
                 yield content
     else:
         resp = await openai.ChatCompletion.acreate(
-            model=model["model_name"],
-            engine=model["model_engine"],
+            model=model_settings["model_name"],
+            engine=model_settings["model_engine"],
             messages=messages,
-            max_tokens=model.get("max_reply_tokens", LLM_MAX_REPLY_TOKENS),
+            max_tokens=model_settings.get("max_reply_tokens", LLM_MAX_REPLY_TOKENS),
         )
         yield resp["choices"][0]["message"]["content"].strip()
-    
+
 
 async def call_deepinfra(
-    method: str,
-    model: dict,
     messages: list,
-    stream: bool = False,
+    model_settings: dict,
 ):
     try:
         api_key = os.getenv("DEEPINFRA_API_KEY")
-        api_base = os.getenv("DEEPINFRA_API_BASE")
+        api_base = os.getenv("DEEPINFRA_API_BASE").rstrip("/")
     except KeyError as e:
         raise RuntimeError(f"Missing environment variable: {e}")
     
+    method = "POST"
+    url=f"{api_base}/{model_settings['model_name']}"
+    stream = model_settings.get("stream", False)
     
+    headers = {
+        "Authorization": f"bearer {api_key}",
+        "Content-Type": "application/json",
+    }
+
+    data = {
+        "input": convert_openai_messages_to_prompt(messages),
+        "max_new_tokens": model_settings.get("max_reply_tokens", LLM_MAX_REPLY_TOKENS),
+        "stream": stream,
+    }
+
+    if stream:
+        async for chunk in _call_api(
+            method=method,
+            url=url,
+            headers=headers,
+            data=data,
+            stream=stream,
+        ):
+            if chunk.startswith("data: "):
+                chunk = json.loads(chunk[6:])
+                content = chunk.get("token", {}).get("text", None)
+                if content == "</s>":   # Special eos token
+                    break
+                if content is not None:
+                    yield content
+    else:
+        resp = await _call_api(
+            method=method,
+            url=url,
+            headers=headers,
+            data=data,
+            stream=stream,
+        )
+        yield resp["generated_text"].strip()
 
 
 async def _call_api(
@@ -60,4 +111,24 @@ async def _call_api(
     data: dict = {},
     stream: bool = False,
 ):
-    pass
+    async with aiohttp.ClientSession() as session:
+        if stream:
+            async with session.request(
+                method=method,
+                url=url,
+                headers=headers,
+                params=params,
+                json=data,
+            ) as resp:
+                async for line in resp.content:
+                    chunk = line.decode("utf-8").strip()
+                    yield chunk
+        else:
+            resp = await session.request(
+                method=method,
+                url=url,
+                headers=headers,
+                params=params,
+                json=data,
+            )
+            yield await resp.json()
